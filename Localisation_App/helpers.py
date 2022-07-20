6ed179b6f6bda54be304639c31a9275d71fa796d
Shweta Patil <shwetap@gist.cdacitpark.in>
2022-07-20 18:45:25 +0530

    Slug filter added for resources with 10

--- Localisation_App/helpers.py
+++ Localisation_App/helpers.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 def send_forget_password_email(email, token):
     subject = 'Your forget password link'
-    message = f'Hi, click on this link for reset password http://test-ubuntu20.pune.cdac.in/changePassword/{token}/'
+    message = f'Hi, click on this link for reset password http://127.0.0.1:5555/{token}/'
     print("message", message)
     email_form = settings.EMAIL_HOST_USER
     recipient_list = [email]
